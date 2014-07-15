6f18e054f459a8077d1516eeff7b9fa357f50151
Bhanu Kolli <bhanuprakashkolli@yahoo.com>
2014-07-15 10:54:53 -0700

    WP-Blog Auto Commit

--- backup.sh
+++ backup.sh
@@ -16,7 +16,7 @@ DB_TRANSFORM='s,^home/bhanukolli/backup,database,'
 tar -cvf $BACKUP_DIR/$FILE --transform $WWW_TRANSFORM $WWW_DIR --exclude .git --exclude "*.log" --exclude backup
 mysqldump -u$DB_USER -p$DB_PASS $DB_NAME > $BACKUP_DIR/$DB_FILE
 
-cp -rp $BACKUP_DIR/$DB_FILE $WWW_DIR/
+cp -rfp $BACKUP_DIR/$DB_FILE $WWW_DIR/bhanukolli.com.sql
 
 tar --append --file=$BACKUP_DIR/$FILE --transform $DB_TRANSFORM $BACKUP_DIR/$DB_FILE
 rm $BACKUP_DIR/$DB_FILE
