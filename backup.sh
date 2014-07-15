83a1d6894ed0b628c8e0ee575da4f91144248019
Bhanu Kolli <bhanuprakashkolli@yahoo.com>
2014-07-15 10:37:48 -0700

    WP-Blog Auto Commit

---
+++ backup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+NOW=$(date +"%Y-%m-%d-%H%M")
+FILE="bhanukolli.com.$NOW.tar"
+BACKUP_DIR="/home/bhanukolli/backup"
+WWW_DIR="/home/bhanukolli/public_html/blog"
+
+DB_USER="i606234_wp1"
+DB_PASS="L])66f1tfO47)*6"
+DB_NAME="i606234_wp1"
+DB_FILE="bhanukolli.com.$NOW.sql"
+
+WWW_TRANSFORM='s,^home/bhanukolli/public_html/blog,WP-Blog,'
+DB_TRANSFORM='s,^home/bhanukolli/backup,database,'
+
+
+tar -cvf $BACKUP_DIR/$FILE --transform $WWW_TRANSFORM $WWW_DIR --exclude .git --exclude "*.log" --exclude backup
+mysqldump -u$DB_USER -p$DB_PASS $DB_NAME > $BACKUP_DIR/$DB_FILE
+
+cp -rp $BACKUP_DIR/$DB_FILE $WWW_DIR/
+
+tar --append --file=$BACKUP_DIR/$FILE --transform $DB_TRANSFORM $BACKUP_DIR/$DB_FILE
+rm $BACKUP_DIR/$DB_FILE
+gzip -9 $BACKUP_DIR/$FILE
+
+
+
